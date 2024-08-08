7d708ca334a6b6cbe99d70d7ad082036f4175ed0
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-08-08 19:50:01 -0300

    conexion a postgres

--- database/db.js
+++ database/db.js
@@ -6,8 +6,14 @@ const usuario = process.env.user;
 const password = process.env.passw;
 
 const db = new Sequelize(bd, usuario, password,{
-    host:'127.0.0.1',
-    dialect: 'mysql',
+    host:'ep-long-moon-a4x0q0np-pooler.us-east-1.aws.neon.tech',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // puedes cambiar esto según tu configuración de SSL
+        }
+      }
 })
 
 export default db;
