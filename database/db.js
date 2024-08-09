ab931dc7af121027cb4a23742437c1cb5c04c062
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-08-08 21:09:03 -0300

    fix vercel

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
+import pg from "pg"
 dotenv.config();
 const bd = process.env.BaseDeDatos;
 const usuario = process.env.user;
@@ -8,6 +9,7 @@ const password = process.env.passw;
 const db = new Sequelize(bd, usuario, password,{
     host:'ep-long-moon-a4x0q0np-pooler.us-east-1.aws.neon.tech',
     dialect: 'postgres',
+    dialectModule:pg,
     dialectOptions: {
         ssl: {
           require: true,
