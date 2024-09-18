892ac9ab3b2722e59c80c5f94acb42a1c4c527fc
octaviopacman <70413021+octaviopacman@users.noreply.github.com>
2024-09-18 23:40:50 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 import pg from "pg"
 dotenv.config();
-const bd = process.env.BaseDeDatos;
-const usuario = process.env.user;
-const password = process.env.passw;
+const bd = process.env.POSTGRES_DATABASE;
+const usuario = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
 
 const db = new Sequelize(bd, usuario, password,{
     host:'ep-long-moon-a4x0q0np-pooler.us-east-1.aws.neon.tech',
